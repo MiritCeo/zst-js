87001eef0ea790ca1094c09d36c58a0c908fafac
rajg0v <j.kwiatkowski1337@gmail.com>
2022-11-02 14:00:44 +0100

    kuba

---
+++ kuba/dupa.js
@@ -0,0 +1,71 @@
+
+
+
+
+// food = Number(prompt('Ile bylo jedzenia'))
+// tipPercentage = Number(prompt('tip %?')) / 100
+// tipAmount = food * tipPercentage / total = food + tipAmount
+//  tipAmount = food * tipPercentage
+
+
+//  console.log('tip Amount', tipAmount)
+//  console.log('total', total)
+// alert(TipAmount)
+
+//let name = 'Peter Pan'
+//console.log(name)
+
+
+// weather app
+
+// let weather = prompt('How is the weather?')
+// if (weather == 'rainy') {
+//   console.log('Grab your Umbrella')
+// } else {
+//   console.log('Wear your sunglasess')
+// }
+
+
+// funkcje
+
+// function sayMyName() {
+//     console.log('Kuba')
+
+// }
+// sayMyName()
+
+// function sayMyName2(name) {
+//     console.log(name)
+
+// }
+// // sayMyName2('Kevin')
+
+// function greeting(name) {
+//     greet = 'hi ' + name + ',how are you ?'
+//     console.log(greet)
+//     greet = `Hi ${name}, Nice to meet you BABE !`
+//     console.log(greet)
+// }
+// greeting('Andro')
+
+// function sum(a,b) {
+//     console.log(a + b)
+// }
+
+// num1 = sum(1, 2)
+// console.log(num1)
+
+
+const fruits = ['banan','jablko','pomarancza','gruszka']
+// console.log(fruits)
+
+
+fruits.push('cookie')
+console.log(fruits)
+
+fruits.push('blueberry')
+console.log(fruits)
+
+console.log(fruits.slice(0,3))
+
+
