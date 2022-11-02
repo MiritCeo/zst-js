a5718ec456bd1c72ee141e4fac79f436dfc75cd0
Adam1707 <adam12022007@gmail.com>
2022-11-02 14:15:41 +0100

    miki

---
+++ miki/skrypty.js
@@ -0,0 +1,34 @@
+// let weather = prompt('How is the weather?')
+// if (weather == 'rain' ) {
+//     console.log('grab your umbrella' )
+// } else {
+//     console.log('wear your sunglasses')
+// }
+
+function sayMyName(){
+  console.log("Qazi")
+}
+
+
+
+function sayMyName2(Kevin) {
+    console.log(name)
+  }
+
+  
+function greeting (name) {
+ //greet = 'hi' + name + ', Nice to meet you!'
+ //onsole.log(greet)
+ greet = ` hi ${name}, Nice to meet you!` 
+ console.log(greet)
+}
+
+greeting ('Johnny Deep')
+
+function sum(a, b) {
+return a + b
+}
+
+num1 = sum(1, 2)
+console.log(num1)
+
