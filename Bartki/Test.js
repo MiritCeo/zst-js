090e3f04761070574367b949c52eb92e2cd3656e
kobqa <babieganski2@gmail.com>
2022-11-02 13:52:39 +0100

    js.test

---
+++ Bartki/Test.js
@@ -0,0 +1,88 @@
+
+// food = prompt('ile kosztowało')
+// tipPercentage = prompt('tip %?') / 100
+// tipAmount = food * tipPercentage
+
+// console.log(tipAmmout)
+
+
+// function SayMyName(name) {
+//     console.log(name)
+// }
+
+// SayMyName('Andrzej')
+
+// function SayMyName2(name) {
+//     console.log(name)
+// }
+
+// SayMyName2('Jurek')
+
+// function greeting(name) {
+//     greet = `Siema ${name}, jesteś zjebany`
+//     console.log(greet)
+// }
+
+// greeting('Paweł')
+
+// function sum(a, b){
+//     return a + b
+// }
+
+// let weather = prompt('Jaka jest pogoda?')
+// if (weather == 'deszczowa'){
+//     console.log('Ukradnij od starszej pani parasol');
+// }
+// else {
+//     console.log('Ukradnij od kogoś okulary przewisłoneczne');
+// }
+
+
+// function kalkulatorfood(food, tip) {
+// tipPercentage =  tip / 100
+// tipAmount = food * tipPercentage
+// total = sum(food, tipAmount)
+// return total
+// }
+// console.log(kalkulatorfood(100, 20))
+
+
+// const sumArrow = (a, b) => {
+//     return a + b 
+// }
+
+// const sumArrow2 = (a, b) => a + b
+
+// const groceries = ['banan', 'krowa', 'twoja stara', 'butla z gazem']
+// console.log(groceries)
+// console.log(groceries[3])
+
+
+// const monkey = {
+//     name: 'Szymon N.',
+//     skin: 'Black',
+//     iq: '-420',
+//     height: '69kg'
+// }
+
+// console.log(monkey);
+// console.log(monkey.iq);
+
+// const introducer = (name, skin) => {
+//     const person = {
+//         name: name,
+//         skin: skin
+//     }
+//     const intro = `Hello, maj nejm is ${person.name} ent de kolor of maj skin is ${person.skin}`
+
+//     return intro
+// }
+
+// console.log(introducer('Monke', 'black'))
+ 
+const fruits = ['banan', 'gumball', 'darwin', 'analiese', 'Nicole', 'Richard', 'banan', 'gumball', 'darwin', 'analiese', 'Nicole', 'Richard', 'banan', 'gumball', 'darwin', 'analiese', 'Nicole', 'Richard']
+
+
+for (let g=0; g < fruits.length; g++) {
+    console.log(fruits[g])
+}
