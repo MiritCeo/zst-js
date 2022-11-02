7d568ac68014c32da9f6c8ed65fce96ec3fced1a
zer0shka <sosiskaromanpozila@gmail.com>
2022-11-02 14:07:20 +0100

    npc

---
+++ npc/podstawy.js
@@ -0,0 +1,66 @@
+// //variables
+// let name = prompt
+// console.log(name)
+
+// let sentence = 'how are you doing today, nice to see you, hope you have a great day'
+// operators
+// fruit = prompt('what is your favorite fruit?')
+
+// console.log(fruit)
+
+// * / ** + -
+
+// food = Number(prompt('how much was the food?'))
+// tipPercentage = Number(prompt('tip %?')) / 100
+// tipAmount = food * tipPercentage
+// total = food + tipAmount
+
+// console.log('tip amo 
+
+//data types (strings, numbers)
+
+
+//Baby weather app (conditionals)
+//if rain -> 'Grab your umbrella
+// let weather = prompt('How is the weather?')
+// if (weather == 'rain') {
+//     console.log('Grab your umbrella')
+// } else if (weather == 'sunny') 
+//     console.log('Wear yours sunglasses') 
+// else if (weather == 'windy')
+//     console.log('Wear your scarf')
+// else {
+//     console.log('something else')
+// }
+     
+
+
+
+// function 
+// function sayMyName(){
+//     console.log('Heisenberg')
+// }
+
+// sayMyName();
+ 
+// function sayMyName2(name){
+//     console.log(name)
+// }
+// sayMyName2('Skyler') 
+
+// function greeting(name){
+//     console.log(`hi ${name}, Nice to meet you!`)
+// }
+
+//greeting('Heisenberg')
+
+//Type casting
+
+function sum(a,b) {
+    // return 
+    return a + b 
+}
+
+console.log(sum (1,2))
+num1 = sum(1,2)
+console.log(num1)
