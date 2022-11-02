60a4f12bcd1c31fc91ff8fe531382cb0c28a033e
blapiotr <1bgza.3@gamil.com>
2022-11-02 14:14:36 +0100

    blapiotr

---
+++ blapiotr/skrypty.js
@@ -0,0 +1,29 @@
+console.log('hello world')
+console.log('Rafeh Quazi')
+
+let name = 'Szymon Gejus'
+console.log(name)
+
+let sentence = 'jak sie dzisiaj czujesz, dobrze cie widziec, zycze ci milego dnia!'
+
+fruit = prompt('jaki jest twoj ulubiony owoc?')
+
+console.log(fruit)
+
+food = Number(prompt('how much was the food?'))
+console.log(food)
+tipPercentage = Number(prompt('tip %?')) / 100
+tipAmount = food * tipPercentage
+total = food + tipAmount
+
+console.log(tipAmount)
+console.log('total' , total)
+alert(tipAmount)
+
+let weater = 'sunny'
+
+if (weather == 'rain') {
+    console.log('Grab your umbrella ☔')
+    } else {
+        console.log('Wear your sunglasses 😎')
+    }
